76f3ae913f4fd005c59a9b7b4ea982b964eeb845
hasnatahmad1 <abulhasnatahmad12@gmail.com>
2026-01-15 17:32:13 +0500

    commit chatbot-proper-project

---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import LoadingSpinnerImage from '../assets/loading-spinner.gif'
+import { Chatbot } from 'supersimpledev'
+import './ChatInput.css'
+
+
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+    const [inputText, setInputText] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+
+
+    function saveInputText(event) {
+        setInputText(event.target.value);
+    }
+
+    async function sendMessage() {
+        if (inputText === '') {
+            return
+        }
+
+        if (isLoading === true) {
+            alert('First Response already in progress')
+            return;
+        }
+
+        setIsLoading(true);
+
+        const newChatMessages = [
+            ...chatMessages, {
+                message: inputText,
+                sender: 'user',
+                id: crypto.randomUUID()
+            }
+        ]
+        setChatMessages(newChatMessages);
+
+        setInputText('');
+
+        setChatMessages(
+            [
+                ...newChatMessages, {
+                    message: (<img
+                        className="loading-spinner-image"
+                        src={LoadingSpinnerImage}
+                    />),
+                    sender: 'robot',
+                    id: crypto.randomUUID()
+                }
+            ]
+        );
+
+        const response = await Chatbot.getResponseAsync(inputText);
+        setChatMessages(
+            [
+                ...newChatMessages, {
+                    message: response,
+                    sender: 'robot',
+                    id: crypto.randomUUID()
+                }
+            ]
+        );
+
+        setIsLoading(false)
+
+    }
+
+    function eventControlling(event) {
+        //console.log(event.key)
+        if (event.key === 'Enter') {
+            sendMessage();
+        }
+        if (event.key === 'Escape') {
+            setInputText('');
+        }
+    }
+
+
+    return (
+        <div className="chat-input-container">
+            <input
+                placeholder="Send a message to Chatbot"
+                size="30"
+                onChange={saveInputText}
+                onKeyDown={eventControlling}
+                value={inputText}
+                className="chat-input"
+            />
+            <button
+                onClick={sendMessage}
+                className="send-button"
+            >Send</button>
+        </div>
+    );
+}
---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,51 @@
+import RobotProfileImage from '../assets/robot.png'
+import UserProfileImage from '../assets/user.png'
+import './ChatMessage.css'
+
+
+//ye wo component h jb user koi message kre ga or jo us ko receive ho ga.
+export function ChatMessage({ message, sender }) {
+    //console.log(props);
+    //console.log(props.message);
+
+    //const message = props.message;
+    //const sender = props.sender;
+
+    // shortcut for doing this upar wali cheez yr:
+    //const { message, sender } = props;
+
+    /*
+    if (sender === 'user') {
+        return (
+            <div>
+                {message}
+                <img src="../user.png" width="50" />
+            </div>
+        );
+    } else if (sender === 'robot') {
+        return (
+            <div>
+                <img src="../robot.png" width="50" />
+                {message}
+            </div>
+        );
+    }
+    */
+
+    return (
+        <div className={sender === 'user'
+            ? "chat-message-user"
+            : "chat-message-robot"
+        }>
+            {sender === 'robot' && (
+                <img src={RobotProfileImage} className="chat-message-profile" />
+            )}
+            <div className="chat-message-text">
+                {message}
+            </div>
+            {sender === 'user' && (
+                <img src={UserProfileImage} className="chat-message-profile" />
+            )}
+        </div>
+    );
+}
